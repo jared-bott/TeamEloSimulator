fd779be1f75d7607d47a7a53033c4378804aa3fd
Jared N. Bott <jared.bott@gmail.com>
2016-02-08 00:10:02 -0500

    Created an interface and class for choosing players from a list of players

---
+++ EloSimulator/PlayerFinders/SimplePlayerFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EloSimulator
+{
+    /// <summary>
+    /// Find players within an Elo range
+    /// </summary>
+    public class SimplePlayerFinder : IPlayerFinder
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="players"></param>
+        /// <param name="maxElo"></param>
+        /// <param name="eloRange"></param>
+        /// <returns></returns>
+        public List<Player> FindPlayers( List<Player> players, double maxElo, int eloRange )
+        {
+            List<Player> possible = players.FindAll( x => x.GetElo() <= maxElo && x.GetElo() >= maxElo - eloRange );
+
+            return possible;
+        }
+    }
+}
